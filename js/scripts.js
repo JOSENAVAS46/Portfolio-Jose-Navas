63e50661f7a86266d96750b479defce6920d3bfe
Jose Francisco Navas Ordoñez <85124451+JOSENAVAS46@users.noreply.github.com>
2022-10-07 16:23:43 -0500

    Grandes Cambios
    Multiidioma agregado, Modificacion de ubicaciones, Redes sociales

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,40 @@ const toggleColors = document.getElementById("toggle_colors");
 
 const rootStyles = document.documentElement.style;
 
-toggleTheme.addEventListener("click",()=>{
+const flagsElement = document.getElementById("flags");
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (language) => {
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
+
+    for(const textToChange of textsToChange){
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+
+        textToChange.textContent = texts[section][value];
+    }  
+};
+
+flagsElement.addEventListener("click", (e) => {
+    changeLanguage(e.target.dataset.language);
+});
+
+
+toggleTheme.addEventListener("click", () => {
     document.body.classList.toggle("dark")
-    if(toggleIcon.src.includes("moon.svg")){
-        toggleIcon.src= "assets/icons/sun.svg";
+    if (toggleIcon.src.includes("moon.svg")) {
+        toggleIcon.src = "assets/icons/sun.svg";
         toggleText.textContent = "Ligth Mode";
     }
-    else{
-        toggleIcon.src= "assets/icons/moon.svg";
+    else {
+        toggleIcon.src = "assets/icons/moon.svg";
         toggleText.textContent = "Dark Mode";
     }
 
 });
 
-toggleColors.addEventListener("click",(e)=>{
+toggleColors.addEventListener("click", (e) => {
     rootStyles.setProperty("--primary-color", e.target.dataset.color);
 });
